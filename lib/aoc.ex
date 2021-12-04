f92053012bc06b62415764acb0e80d3665eb98c2
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2021-12-04 16:24:00 +0100

    Add main runner

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -1,18 +1,4 @@
 defmodule Aoc do
-  @moduledoc """
-  Documentation for `Aoc`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def run do
   end
 end
