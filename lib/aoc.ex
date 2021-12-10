2847904f495fc36e882ef55080132977c267f32a
Kenneth Kostresevic <kenneth.kostresevic@verybigthings.com>
2021-12-10 16:24:23 +0100

    Run sevent segment search in main runner

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -1,9 +1,10 @@
 defmodule Aoc do
-  alias Aoc.Puzzles.{BinaryDiagnostic, Dive, SonarSweep}
+  alias Aoc.Puzzles.{BinaryDiagnostic, Dive, SevenSegmentSearch, SonarSweep}
 
   def run do
     SonarSweep.run()
     Dive.run()
     BinaryDiagnostic.run()
+    SevenSegmentSearch.run()
   end
 end
